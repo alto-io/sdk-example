c506dc0aba764a2e6dad6c501f41a35a16b676e0
inn3rGames <hi@inn3r.com>
2021-12-13 14:57:27 +0200

    Added Basic Game.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { ethers } from "ethers";
 import axios from "axios";
 import Phaser from "phaser";
+import background from "./assets/background.png";
+import spike from "./assets/spike.png";
 
 if (typeof window.ethereum !== "undefined") {
   let connectButton = document.createElement("button");
@@ -81,22 +83,93 @@ if (typeof window.ethereum !== "undefined") {
               }
 
               preload() {
+                this.load.image("background", background);
+                this.load.image("spike", spike);
                 this.load.image("useAvatar", myAvatar.src);
               }
 
-              create() {
-                let currentAvatar = this.add.image(200, 200, "useAvatar");
-                currentAvatar.setOrigin(0.5, 1);
-                currentAvatar.setScale(0.25, 0.25);
+              fly() {
+                this.currentAvatar.body.velocity.y = -300;
+              }
 
-                this.tweens.add({
-                  targets: currentAvatar,
-                  y: 400,
-                  duration: 2000,
-                  ease: "Power2",
-                  yoyo: true,
-                  loop: -1,
+              getRightmostSpike() {
+                let rightmostSpike = 0;
+                this.spikeGroup.getChildren().forEach(function (spike) {
+                  rightmostSpike = Math.max(rightmostSpike, spike.x);
                 });
+                return rightmostSpike;
+              }
+
+              placeSpikes(addScore) {
+                let rightmost = this.getRightmostSpike();
+                let spikeHolePosition = Phaser.Math.Between(-100, 100);
+                this.spikePool[0].x =
+                  rightmost +
+                  this.spikePool[0].getBounds().width +
+                  Phaser.Math.Between(220, 280);
+                this.spikePool[0].y = 0 + spikeHolePosition;
+                this.spikePool[0].setOrigin(0.5, 0.5);
+                this.spikePool[0].scaleY = -1;
+                this.spikePool[1].x = this.spikePool[0].x;
+                this.spikePool[1].y =
+                  this.game.scale.height + spikeHolePosition;
+                this.spikePool[1].setOrigin(0.5, 0.5);
+                this.spikePool = [];
+                if (addScore) {
+                  //this.updateScore(1);
+                }
+              }
+
+              create() {
+                this.backgroundImage = this.add.image(
+                  this.game.scale.width * 0.5,
+                  this.game.scale.height * 0.5,
+                  "background"
+                );
+
+                this.currentAvatar = this.physics.add.sprite(
+                  this.game.scale.width * 0.5,
+                  this.game.scale.height * 0.5,
+                  "useAvatar"
+                );
+                this.currentAvatar.body.gravity.y = 800;
+                this.currentAvatar.setOrigin(0.5, 0.5);
+                this.currentAvatar.setScale(0.2, 0.2);
+                this.input.on("pointerdown", this.fly, this);
+
+                /* let downSpike = this.add.image(
+                  this.game.scale.width * 0.8,
+                  this.game.scale.height * 1,
+                  "spike"
+                );
+
+                this.topSpike = this.add.image(
+                  this.game.scale.width * 0.8,
+                  this.game.scale.height * 0,
+                  "spike"
+                );
+
+                this.topSpike.setRotation(Math.PI); */
+
+                this.spikeGroup = this.physics.add.group();
+                this.spikePool = [];
+                for (let i = 0; i < 4; i++) {
+                  this.spikePool.push(this.spikeGroup.create(0, 0, "spike"));
+                  this.spikePool.push(this.spikeGroup.create(0, 0, "spike"));
+                  this.placeSpikes(false);
+                }
+                this.spikeGroup.setVelocityX(-125);
+              }
+
+              update() {
+                this.spikeGroup.getChildren().forEach(function (spike) {
+                  if (spike.getBounds().right < 0) {
+                    this.spikePool.push(spike);
+                    if (this.spikePool.length == 2) {
+                      this.placeSpikes(true);
+                    }
+                  }
+                }, this);
               }
             }
 
