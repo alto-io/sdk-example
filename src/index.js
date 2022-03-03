2ebbb7b22e38972631570bd373170e146451f32a
inn3rGames <hi@inn3r.com>
2022-03-03 06:25:26 +0200

    Use latest SDK version

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import  Home from "./home";
 import Game from "./game";
 import End from "./end";
 
-const testMode = false;
-const testAddress = "0xf0103243f4d22b5696588646b21313d85916a16a";
-const nftsLimit = 50;
-const gameId = "0cd69241-531c-4698-bf17-454dd6cb1ab4";
-const apiUrl = "https://lb-dev.gmfrens.games";
+let settings = {
+  testMode: false, // Test mode.
+  testAddress: "0xf0103243f4d22b5696588646b21313d85916a16a", // The test address you would like to use.
+  nftsLimit: 50, // Limit the maximum numbers of fetched NFTs.
+  gameId: "0cd69241-531c-4698-bf17-454dd6cb1ab4", // Your gameId provided by us.
+  apiUrl: "https://lb-dev.gmfrens.games", // Your apiUrl provided by us.
+};
+export const arc = new Arcadians(settings);
 
-export const arc = new Arcadians(testMode, testAddress, nftsLimit, gameId, apiUrl);
 
 const config = {
   type: Phaser.AUTO,
